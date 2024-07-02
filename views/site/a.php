1f1f1d6eb3d68aff216211b80a3f618dc1cae24c
volodymyr99 <492145768@ukr.net>
2024-07-02 09:22:23 +0300

    1st commit

---
+++ views/site/a.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\helpers\Html;
+
+$this->title = 'A';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        ************
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
