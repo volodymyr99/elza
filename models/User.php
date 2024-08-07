8243c164cc3ea13ef6659008cc7c75f56f49eb3a
volodymyr99 <492145768@ukr.net>
2024-08-07 23:36:27 +0300

    Change User.php for autorization from db

--- models/User.php
+++ models/User.php
@@ -2,13 +2,18 @@
 
 namespace app\models;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+use yii\db\ActiveRecord;
+
+
+class User extends ActiveRecord implements \yii\web\IdentityInterface
 {
+/*
     public $id;
     public $username;
     public $password;
     public $authKey;
     public $accessToken;
+
 
     private static $users = [
         '100' => [
@@ -28,77 +33,55 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
     ];
 
 
+    const STATUS_DELETED = 0;
+    const STATUS_INACTIVE = 9;
+    const STATUS_ACTIVE = 10;
+*/
+
+    const STATUS_DELETED = 9;
+    const STATUS_INACTIVE = 1;
+    const STATUS_ACTIVE = 0;
+
     /**
      * {@inheritdoc}
      */
+    public static function tableName()
+    {
+        return '{{user}}';
+    }
+
     public static function findIdentity($id)
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        return static::findOne($id);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return static::findOne(['access_token' => $token]);
     }
 
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
     public static function findByUsername($username)
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return static::findOne(['username' => $username]);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getAuthKey()
     {
-        return $this->authKey;
+        return $this->auth_key;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function validateAuthKey($authKey)
     {
-        return $this->authKey === $authKey;
+        return $this->auth_key === $authKey;
     }
 
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
     public function validatePassword($password)
     {
-        return $this->password === $password;
+        return \Yii::$app->security->validatePassword($password, $this->password_hash);
     }
 }
